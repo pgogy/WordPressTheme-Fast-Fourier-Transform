37c1c8d8a7940b35af185b99e345c8c967097778
patlockley <patrick.lockley@googlemail.com>
2016-06-14 10:55:03 +0100

    Removed old files

--- js/widgetmasonry.js
+++
@@ -1,9 +0,0 @@
-jQuery(window).load(function() {
-        
-    var container = document.querySelector('#widget-area-bottom');
-    var msnry = new Masonry( container, {
-        itemSelector: '.masonry',
-        columnWidth: '.masonry',                
-    });  
-      
-});
